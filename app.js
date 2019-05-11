20095964f32b98ae58b26e82d3042252723d38e2
igor popenov <ipopenov@protonmail.com>
2019-05-11 18:20:19 -0400

    Added comments and completed exercises through lesson 17

--- app.js
+++ app.js
@@ -147,52 +147,274 @@
 //////////////////// Lesson 9 - Events /////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-let h2 = document.querySelector('#book-list h2')
+/* ========================================================== */
+/* Lesson 9.1 - This selects the h2 tag and adds an eventListener to it. Every time the h2 tag is clicked, it logs the event in the console. */
+/* ========================================================== */
 
-/* first parameter is the click event.  E is the event parameter */
-h2.addEventListener('click', function(e){
-  /* Tells us which element was clicked. What was the target of the event */
-  console.log(e.target);  
-  /* just the event object itself to browse through it */
-  console.log(e);
-  /* lets us know if the shift key was pressed down during the even */
-  console.log(e.shiftKey);
-})
+// let h2 = document.querySelector('#book-list h2')
+// /* first parameter is the click event.  E is the event parameter */
+// h2.addEventListener('click', function(e){
+//   /* Tells us which element was clicked. What was the target of the event */
+//   console.log(e.target);  
+//   /* just the event object itself to browse through it */
+//   console.log(e);
+//   /* lets us know if the shift key was pressed down during the even */
+//   console.log(e.shiftKey);
+// })
 
-/* The code below lets us delete */
+// /* ========================================================== */
+// /* Lesson 9.2 - The code below adds an event listener to each delete btn and allows us to delete the list item.  Uses a traversing method to go up to the UL and delete child LI. */
+// /* ========================================================== */
 
-let btns = document.querySelectorAll('#book-list .delete');
+// let btns = document.querySelectorAll('#book-list .delete');
 
-Array.from(btns).forEach(function(btn){
-  btn.addEventListener('click', function(e){
+// /* Do not have to turn this into an Array since 'btns' is already a node list */
+// Array.from(btns).forEach(function(btn){
+//   btn.addEventListener('click', function(e){
 
-    const li = e.target.parentElement;
+//     /* Grabs the parentElement of whichever btn was clicked */
+//     const li = e.target.parentElement;
 
-    li.parentNode.removeChild(li);
-  })
-});
+//     /* Removes child from the UL list */
+//     li.parentNode.removeChild(li);
+//   })
+// });
 
-const link = document.querySelector('#page-banner a');
+// /* ========================================================== */
+// /* Lesson 9.3 - Prevent Default method. Added a link to the html that links to the net ninja.  The code below will prevent the browser from firing off the default method associated with a link and instead do another event such as print to console  */
+// /* ========================================================== */
 
-link.addEventListener('click', function(e){
-  e.preventDefault();
-  console.log("navigation to: ", e.target.textContent, "was prevented")
-})
+// /* added in a link to the html href "the net ninja" */
+
+// /* created a const from the newly addd html link*/
+// const link = document.querySelector('#page-banner a');
+
+
+// link.addEventListener('click', function(e){
+//   /* Prevents default action from occuring */
+//   e.preventDefault();
+
+//   /* Instead does the action below: */
+//   console.log("navigation to: ", e.target.textContent, "was prevented")
+// })
 
 ////////////////////////////////////////////////////////////////////////////////
 //////////////////// Lesson 10 - Event Bubbling ////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
+// /* Event bubbling is used to assing action to parentElements so that additional books can be added and would not need code associated with the button but would inherit it from the parentElement  */
 
 // const list = document.querySelector('#book-list ul');
-
+// // delete books
 // list.addEventListener('click', function(e){
 //   if (e.target.className == 'delete'){
 //     const li = e.target.parentElement;
-//     // li.parentNode.removeChild(li);  // Can be written this way but list is already grabbing li.parentNode
+//     /* Can be written this way but list is already grabbing li.parentNode*/
+//     // li.parentNode.removeChild(li);  
+    
+
 //     list.removeChild(li);
 //   }
 // })
+
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////// Lesson 11 - Interacting with Forms ////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// /* Grabs all the forms on the document and puts them into HTML Collection */
+// console.log(document.forms[0]);
+// console.log(document.forms['add-book']);
+
+// //add book
+
+// const addForm = document.forms['add-book'];
+
+// addForm.addEventListener('submit', function(e){
+//   e.preventDefault();
+//   const value = addForm.querySelector('input[type="text"]').value;
+//   console.log(value);
+
+// });
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////// Lesson 12 - Creating Elements /////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// /* Grabs all the forms on the document and puts them into HTML Collection */
+// console.log(document.forms[0]);
+// console.log(document.forms['add-book']);
+
+// //add book
+
+// const addForm = document.forms['add-book'];
+
+// addForm.addEventListener('submit', function(e){
+//   e.preventDefault();
+//   const value = addForm.querySelector('input[type="text"]').value;
+
+//   // create elements
+//   const li = document.createElement("li");
+//   const bookName = document.createElement("span");
+//   const deleteBtn = document.createElement("span");
+
+//   // add content
+//   deleteBtn.textContent = 'delete';
+//   bookName.textContent = value;
+
+//   // append to document
+//   li.appendChild(bookName);
+//   li.appendChild(deleteBtn);
+//   list.appendChild(li);
+
+// });
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////// Lesson 13 - Styles and Classes ////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// /* Grabs all the forms on the document and puts them into HTML Collection */
+// console.log(document.forms[0]);
+// console.log(document.forms['add-book']);
+
+// //add book
+
+// const addForm = document.forms['add-book'];
+
+// addForm.addEventListener('submit', function(e){
+//   e.preventDefault();
+//   const value = addForm.querySelector('input[type="text"]').value;
+
+//   // create elements
+//   const li = document.createElement("li");
+//   const bookName = document.createElement("span");
+//   const deleteBtn = document.createElement("span");
+
+//   // add content
+//   deleteBtn.textContent = 'delete';
+//   bookName.textContent = value;
+
+//   // add classes 
+//   bookName.classList.add('name');
+//   deleteBtn.classList.add('delete');
+
+
+//   // append to document
+//   li.appendChild(bookName);
+//   li.appendChild(deleteBtn);
+//   list.appendChild(li);
+
+// });
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////// Lesson 14 - Attributes ////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// let book = document.querySelector('li:first-child .name');
+
+// /* Gets attribute*/
+// let test1 = book.getAttribute('class');
+// console.log(test1)
+
+// /* Doesnt get attribute because this attribute doesnt exist */
+// let test2 = book.getAttribute('href');
+// console.log(test2);
+
+// /* Sets attribute and over rides existing attribute */
+// let test3 = book.setAttribute('class', 'name-2')
+// console.log(book);
+
+// /* checks to see if there is an attribute */
+// let test4 = book.hasAttribute('href')
+// console.log(test4)
+
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////// Lesson 15 - Checkboxes & Change Events ////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// const list = document.querySelector('#book-list ul');
+
+// // hide books
+// const hideBox = document.querySelector("#hide");
+
+// hideBox.addEventListener('change', function(e) {
+//   if(hideBox.checked){
+//     list.style.display = "none";
+//   } else {
+//     list.style.display = "initial";
+//   }
+// });
+
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////// Lesson 16 - Custom Search Filter //////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+// const list = document.querySelector('#book-list ul');
+
+// // filter books
+// const searchBar = document.forms['search-books'].querySelector('input');
+// searchBar.addEventListener('keyup',function(e){
+//   const term = e.target.value.toLowerCase();
+//   const books = list.getElementsByTagName('li');
+//   Array.from(books).forEach(function(book){
+//     const title = book.firstElementChild.textContent;
+//     if(title.toLowerCase().indexOf(term) != -1){
+//       book.style.display = 'block';
+//     } else {
+//       book.style.display = 'none';
+//     }
+//   })
+// })
+
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////// Lesson 17 - Tabbed Content ////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+/* Added this to HTML: 
+<div id="tabbed-content">
+					<ul class="tabs">
+						<li data-target="#about" class="active">About</li>
+						<li data-target="#contact">Contact</li>
+					</ul>
+					<div class="panel active" id="about">
+						<p>Content for about tab...</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id nunc porta urna ornare rhoncus. Ut convallis ante at.</p>
+					</div>
+					<div class="panel" id="contact">
+						<p>Content for contact tab...</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id nunc porta urna ornare rhoncus. Ut convallis ante at.</p>
+					</div>
+				</div>
+*/
+
+const list = document.querySelector('#book-list ul');
+
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+
+tabs.addEventListener('click', function(e){
+  if (e.target.tagName == "LI"){
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    panels.forEach(function(panel){
+      if (panel == targetPanel){
+        panel.classList.add('active');
+      } else {
+        panel.classList.remove('active');
+      }
+    })
+  }
+})
+
+
+
+
+
+
+
 
 
 
